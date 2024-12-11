bdfb9501f460aed2c5763065cda65f7c61ed2014
adarsh1o <myreality75@gmail.com>
2024-12-11 15:26:35 +0530

    binary search using recursion

--- src/Q704.java
+++ src/Q704.java
@@ -13,17 +13,15 @@ public class Q704 {
 
     }
     public static int tag(int []nums, int high ,int low ,int target){
-        while(high>=low)
-        {
-            int mid=low+(high-low)/2;
-            if (nums[mid]==target){
+        while(high>=low) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
                 return mid;
             }
-            if(nums[mid]>target){
-                return tag(nums,mid-1,low,target);
-            }
-            else{
-                return tag(nums,high,mid+1,target);
+            if (nums[mid] > target) {
+                return tag(nums, mid - 1, low, target);
+            } else {
+                return tag(nums, high, mid + 1, target);
             }
         }
         return -1;
